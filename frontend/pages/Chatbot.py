ba740348f89f9f9b13035ef88c1479c91f2a695f
Padmakumar <padmakumarr21cb@psnacet.edu.in>
2023-09-11 05:16:34 +0000

    Final message

--- frontend/pages/Chatbot.py
+++ frontend/pages/Chatbot.py
@@ -1,28 +1,28 @@
 import streamlit as st
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import Conversation, pipeline
 
-# Load the GPT-2 model and tokenizer
-model_name = "gpt2"
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-model = GPT2LMHeadModel.from_pretrained(model_name)
+# Create a chatbot UI
+st.title("DocBot")
 
-# Add a header/banner
-st.title("Health ChatBot")
+# Initialize the pipeline
+pipe = pipeline("conversational", model="microsoft/DialoGPT-medium")
 
-# Create a text input for user messages with some styling
-user_input = st.text_area("You:", "", key="user_input", height=100)
+# Create an empty conversation to start
+conversation = Conversation()
 
+# Get user input from the user
+user_input = st.text_input("User", "Type your message here")
+
+# Add the user's message to the conversation
+conversation.add_user_input(user_input)
+
+# Check if the user input is not empty
 if user_input:
-    # Encode the user's message
-    input_ids = tokenizer.encode(user_input, return_tensors="pt")
-
-    # Generate a response using the GPT-2 model
-    with st.spinner("ChatBot is thinking..."):
-        response_ids = model.generate(input_ids, num_return_sequences=1)
-
-    # Decode and display the chatbot's response
-    chatbot_response = tokenizer.decode(response_ids[0], skip_special_tokens=True)
+    # Get the bot's response
+    bot_output = pipe(conversation)
     
-    # Display user message and chatbot response in chat history
-    st.text("User: " + user_input)
-    st.text("HealthAssist: " + chatbot_response)
+    # Get the generated text from the bot's response
+    bot_response = bot_output.generated_responses[0]
+    
+    # Display the bot's response
+    st.text("Health Bot: " + bot_response)
--- frontend/pages/Pill_Quality.py
+++ frontend/pages/Pill_Quality.py
@@ -7,75 +7,66 @@ import io
 
 st.title('Pill Visual Quality Checker')
 
-ipex_tab, help_tab = st.tabs(["Intel Extension for PyTorch"])
+ipex_tab = st.tabs(["Intel Extension for PyTorch"])
 
 # Start of Intel Extension for PyTorch Implementation
 
-with ipex_tab:
-    col11, col22 = st.columns(2)
+st.divider()
 
-    with col11:
-        st.empty()
-    with col22:
-        st.empty()
+st.markdown('#### Model Training')
 
-    st.divider()
+data_folder = st.text_input('Root Training Data Folder', value='./store/datasets/medication_qaqc/data/')
+model_path = st.text_input('Save Model Path', value='./store/models/medication_qaqc/model_ipex.h5')
+neg_class = st.number_input('Passing Quality Label', min_value=0, max_value=100)
+learning_rate = st.slider('Learning Rate', min_value=0.0001, max_value=0.05, step=.0001, value=.025, format='%f')
+epochs = st.number_input('Epochs', min_value=1, max_value=100, step=1, value=5)
+data_aug = st.checkbox('Augment Training Data')
 
-    st.markdown('#### Model Training')
+if st.button('Train Model', key='ipex training'):
+    # build request
 
-    data_folder = st.text_input('Root Training Data Folder', value='./store/datasets/medication_qaqc/data/')
-    model_path = st.text_input('Save Model Path', value='./store/models/medication_qaqc/model_ipex.h5')
-    neg_class = st.number_input('Passing Quality Label', min_value=0, max_value=100)
-    learning_rate = st.slider('Learning Rate', min_value=0.0001, max_value=0.05, step=.0001, value=.025, format='%f')
-    epochs = st.number_input('Epochs', min_value=1, max_value=100, step=1, value=5)
-    data_aug = st.checkbox('Augment Training Data')
+    URL = 'http://medication_qaqc:5002/train-ipex'
+    DATA = {"data_folder": data_folder, "neg_class": neg_class, "modeldir": model_path,
+            "learning_rate": learning_rate, "epochs": epochs, "data_aug": data_aug}
+    TRAINING_RESPONSE = requests.post(url=URL, json=DATA)
 
-    if st.button('Train Model', key='ipex training'):
-        # build request
+    if len(TRAINING_RESPONSE.text) < 40:
+        st.error("Model Training Failed")
+        st.info(TRAINING_RESPONSE.text)
+    else:
+        st.success('Training was Successful')
+        st.info('Trained Model Location: ' + str(TRAINING_RESPONSE.json().get('Model Location')))
 
-        URL = 'http://medication_qaqc:5002/train-ipex'
-        DATA = {"data_folder": data_folder, "neg_class": neg_class, "modeldir": model_path,
-                "learning_rate": learning_rate, "epochs": epochs, "data_aug": data_aug}
-        TRAINING_RESPONSE = requests.post(url=URL, json=DATA)
+st.divider()
 
-        if len(TRAINING_RESPONSE.text) < 40:
-            st.error("Model Training Failed")
-            st.info(TRAINING_RESPONSE.text)
-        else:
-            st.success('Training was Successful')
-            st.info('Trained Model Location: ' + str(TRAINING_RESPONSE.json().get('Model Location')))
+st.markdown('#### Intel Extension for PyTorch Inference')
 
-    st.divider()
+data_folder = st.text_input('Root Store', value='./store/datasets/medication_qaqc/data/')
+trained_model_path = st.text_input('Trained Model Path', value='./store/models/medication_qaqc/model_ipex.h5')
+batch_size = st.number_input('Inference Batch Size', min_value=5, max_value=100)
 
-    st.markdown('#### Intel Extension for PyTorch Inference')
+if st.button('Start Batch Evaluation', key='ipex inference'):
+    # build request
+    URL = 'http://medication_qaqc:5002/predict-ipex'
+    DATA = {"trained_model_path": trained_model_path, "data_folder": data_folder, "batch_size": batch_size}
+    INFERENCE_RESPONSE = requests.post(url=URL, json=DATA)
 
-    data_folder = st.text_input('Root Store', value='./store/datasets/medication_qaqc/data/')
-    trained_model_path = st.text_input('Trained Model Path', value='./store/models/medication_qaqc/model_ipex.h5')
-    batch_size = st.number_input('Inference Batch Size', min_value=5, max_value=100)
+    if len(INFERENCE_RESPONSE.text) < 40:
+        st.error("Inference Failed")
+        st.info(INFERENCE_RESPONSE.text)
+    else:
+        st.success('Inference was Successful')
+        classified_pills = INFERENCE_RESPONSE.json().get('Prediction Output')
 
-    if st.button('Start Batch Evaluation', key='ipex inference'):
-
-        # build request
-        URL = 'http://medication_qaqc:5002/predict-ipex'
-        DATA = {"trained_model_path": trained_model_path, "data_folder": data_folder, "batch_size": batch_size}
-        INFERENCE_RESPONSE = requests.post(url=URL, json=DATA)
-
-        if len(INFERENCE_RESPONSE.text) < 40:
-            st.error("Inference Failed")
-            st.info(INFERENCE_RESPONSE.text)
-        else:
-            st.success('Inference was Successful')
-            classified_pills = INFERENCE_RESPONSE.json().get('Prediction Output')
-
-            with st.expander("Pill Classifications"):
-                for label, file in classified_pills:
-                    if label == 1.0:
-                        st.markdown('### Faulty Pill')
-                        st.text(os.path.join(data_folder, 'blind/', file))
-                        image = Image.open(os.path.join(data_folder, 'blind/', file))
-                        st.image(image, width=800)
-                    else:
-                        st.markdown('### Valid Pill')
-                        st.text(os.path.join(data_folder, 'blind/', file))
-                        image = Image.open(os.path.join(data_folder, 'blind/', file))
-                        st.image(image, width=800)
+        with st.expander("Pill Classifications"):
+            for label, file in classified_pills:
+                if label == 1.0:
+                    st.markdown('### Faulty Pill')
+                    st.text(os.path.join(data_folder, 'blind/', file))
+                    image = Image.open(os.path.join(data_folder, 'blind/', file))
+                    st.image(image, width=800)
+                else:
+                    st.markdown('### Valid Pill')
+                    st.text(os.path.join(data_folder, 'blind/', file))
+                    image = Image.open(os.path.join(data_folder, 'blind/', file))
+                    st.image(image, width=800)
